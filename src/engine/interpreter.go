bbb9a13fb83e2e7d7ced58cb488215a96d7e4a3c
burbokop <burbokop@gmail.com>
2020-12-21 15:30:54 +0200

    ff4

--- src/engine/interpreter.go
+++ src/engine/interpreter.go
@@ -2,10 +2,6 @@ package engine
 
 import (
 	"reflect"
-	"sync"
-	"sync/atomic"
-
-	"github.com/enriquebris/goconcurrentqueue"
 )
 
 type Handler interface {
@@ -20,46 +16,31 @@ type Command interface {
 func CommandType() reflect.Type { return reflect.TypeOf((*Command)(nil)).Elem() }
 
 type EventLoop struct {
-	Queue        *goconcurrentqueue.FIFO
-	Active       bool
-	ExecFinished sync.WaitGroup
-	ExecAlive    int32
-}
-
-func (el *EventLoop) Exec() {
-	defer atomic.StoreInt32(&el.ExecAlive, 0)
-	defer el.ExecFinished.Done()
-	if el.Queue == nil {
-		return
-	}
-	for el.Queue.GetLen() > 0 {
-		var cmd, err = el.Queue.Dequeue()
-		if err == nil {
-			cmd.(Command).Execute(el)
-		}
-	}
+	Queue    []Command
+	ExitFlag bool
+	ExitChan chan bool
 }
 
 func (el *EventLoop) Start() {
-	el.Active = true
-	if atomic.CompareAndSwapInt32(&el.ExecAlive, 0, 1) {
-		el.ExecFinished.Add(1)
-		go el.Exec()
+	el.ExitFlag = false
+	el.ExitChan = make(chan bool)
+	go el.Execute()
+}
+
+func (el *EventLoop) Execute() {
+	for !el.ExitFlag || len(el.Queue) > 0 {
+		var cmd Command
+		cmd, el.Queue = el.Queue[0], el.Queue[1:]
+		cmd.Execute(el)
 	}
+	el.ExitChan <- true
 }
 
 func (el *EventLoop) Post(cmd Command) {
-	if el.Queue == nil {
-		el.Queue = goconcurrentqueue.NewFIFO()
-	}
-	el.Queue.Enqueue(cmd)
-	if el.Active && atomic.CompareAndSwapInt32(&el.ExecAlive, 0, 1) {
-		el.ExecFinished.Add(1)
-		go el.Exec()
-	}
+	el.Queue = append(el.Queue, cmd)
 }
 
 func (el *EventLoop) AwaitFinish() {
-	el.Active = false
-	el.ExecFinished.Wait()
+	el.ExitFlag = true
+	<-el.ExitChan
 }
